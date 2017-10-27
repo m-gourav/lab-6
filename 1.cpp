31ec6e159d5a57e215584302e05b90f4d6cce6a7
m-gourav <31315187+m-gourav@users.noreply.github.com>
2017-10-27 23:33:03 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+	using namespace std;
+	main() {
+		int i=0;
+		float f=0;
+		double d=0;
+		char ch='0';
+		int *in;
+		in=&i;
+		float *fl;
+		fl=&f;
+		double *dbl;
+		dbl=&d;
+		char *c;
+		c=&ch;
+		cout<<" \nPRINTING THE SIZE OF PRIMITIVE VARIABLES: ";
+		cout<<" \nSize of INTEGER VARIABLE is: "<<sizeof(i);
+		cout<<" \nSize of FLOAT VARIABLE is: "<<sizeof(f);
+		cout<<" \nSize of DOUBLE VARIABLE is: "<<sizeof(d);
+		cout<<" \nSize of CHARACTER VARIABLE is: "<<sizeof(ch);
+		cout<<" \n \nPRINTING THE SIZE OF PRIMITIVE VARIABLES AS POINTERS:";
+		cout<<" \nSize of INTEGER VARIABLE is: "<<sizeof(in);
+		cout<<" \nSize of FLOAT VARIABLE is: "<<sizeof(fl);
+		cout<<" \nSize of DOUBLE VARIABLE is: "<<sizeof(dbl);
+		cout<<" \nSize of CHARACTER VARIABLE is: "<<sizeof(c);
+		}
